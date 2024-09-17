5caba763b0e5ad306a7a6470de8f7d4744060041
Paige <paige.struthers@gmail.com>
2024-09-17 15:44:53 -0500

    beginner challenge 14 - check if user is logged in OR subscribed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function isLoggedInAndSubscribed(str1, str2) {
-    return str1 === 'LOGGED_IN' && str2 === 'SUBSCRIBED'
+    return str1 === 'LOGGED_IN' || str2 === 'SUBSCRIBED'
 }
 
 console.log(isLoggedInAndSubscribed('LOGGED_OUT', 'SUBSCRIBED'))
