c141973caec22f752de215647a1ec4d3a1ac6bbd
Paige <paige.struthers@gmail.com>
2024-09-18 15:17:25 -0500

    medium challenge 6 - calculate time in mm:ss format

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function progressiveSum(num) {
-    let sum = 0
-    for (let i = 1; i <= num; i++) {
-        sum += i
+function calcTime(seconds) {
+    let minutes = Math.floor(seconds / 60)
+    let secondsRemainder = seconds % 60
+    if (minutes < 10) {
+        minutes = '0' + minutes
     }
-    return sum
+    if (secondsRemainder < 10) {
+        secondsRemainder = '0' + secondsRemainder
+    }
+    return minutes + ':' + secondsRemainder
 }
 
-console.log(progressiveSum(3))
-console.log(progressiveSum(4))
-console.log(progressiveSum(600))
+console.log(calcTime(66)) // -> 01:06
+console.log(calcTime(50)) // -> 00:50
+console.log(calcTime(300)) // -> 05:00
