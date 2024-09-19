638f5bdf8e2dbcf41f3f029a024d9a760b116a3d
Paige <paige.struthers@gmail.com>
2024-09-19 14:31:51 -0500

    advanced challenge 2 - sort an array of numbers

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-
-function showRating(rating) {
-    let string = ''
-    for (let i = 1; i <= Math.floor(rating); i++) {
-        string += "*"
-        if (i !== Math.floor(rating)) {
-            string += ' '
+// Using for loops
+function sortLowToHigh(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[i] < arr[j]) {
+                let min = arr[i]
+                arr[i] = arr[j]
+                arr[j] = min
+            }
         }
     }
-    if (rating === 0.5) {
-        string += '.'
-    }
-    else if (!Number.isInteger(rating)) {
-        string += ' .'
-    }
-    return string
+    return arr
+}
+
+// Using array sort method (preferred)
+function sortLowToHigh(arr) {
+    arr.sort((a, b) => a - b)
+    return arr
 }
 
 
-console.log(showRating(3)) // -> * * *
-console.log(showRating(4.5)) // -> * * * * .
-console.log(showRating(0.5)) // -> .
+console.log(sortLowToHigh([20, 40, 10, 30, 50, 10])) // -> [10, 10, 20, 30, 40, 50]
+console.log(sortLowToHigh([5, 10, 0, -5])) // -> [-5, 0, 5, 10]
+console.log(sortLowToHigh([3, 2, 1, 0])) // -> [0, 1, 2, 3]
